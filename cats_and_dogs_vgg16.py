db79d142d8c24fabb4b1ebddc45a0d9272ff2625
shlee.mars <shlee.mars@gmail.com>
2020-01-07 08:24:39 +0900

    cats and dog vgg16

---
+++ cats_and_dogs_vgg16.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import layers, models, optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from keras.backend import tensorflow_backend as K
+
+# import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+K.set_session(tf.Session(config=config))
+
+original_dataset_dir = '/home/shlee/datasets/cats_and_dogs/train'
+
+base_dir = '/home/shlee/datasets/cats_and_dogs/small'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+if not os.path.isdir(base_dir):
+    os.mkdir(base_dir)
+
+    os.mkdir(train_dir)
+    os.mkdir(validation_dir)
+    os.mkdir(test_dir)
+
+    train_cats_dir = os.path.join(train_dir, 'cats')
+    os.mkdir(train_cats_dir)
+
+    train_dogs_dir = os.path.join(train_dir, 'dogs')
+    os.mkdir(train_dogs_dir)
+
+    validation_cats_dir = os.path.join(validation_dir, 'cats')
+    os.mkdir(validation_cats_dir)
+
+    validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+    os.mkdir(validation_dogs_dir)
+
+    test_cats_dir = os.path.join(test_dir, 'cats')
+    os.mkdir(test_cats_dir)
+
+    test_dogs_dir = os.path.join(test_dir, 'dogs')
+    os.mkdir(test_dogs_dir)
+
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(train_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(validation_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['cat.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(test_cats_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(train_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1000, 1500)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(validation_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+
+    fnames = ['dog.{}.jpg'.format(i) for i in range(1500, 2000)]
+    for fname in fnames:
+        src = os.path.join(original_dataset_dir, fname)
+        dst = os.path.join(test_dogs_dir, fname)
+        shutil.copyfile(src, dst)
+
+from keras.applications import VGG16
+conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 3))
+conv_base.trainable = False
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='binary')
+
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50
+)
+model.save('cats_and_dogs_small_1.h5')
+
+history_dict = history.history
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+epochs = range(1, len(loss) + 1)
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+# plt.xlabel('Epochs')
+
+plt.plot(epochs, acc, 'bo', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+
+plt.show()
